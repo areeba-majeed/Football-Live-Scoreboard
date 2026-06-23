36ea68a12add35356bd3d7ad2fc58aa94727a97a
areebamajeed212@gmail.com <areebamajeed212@gmail.com>
2026-06-23 22:09:29 +0500

    completing frontend and documentation

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import { MatchesProvider } from "./context/MatchesContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { SettingsProvider } from "./context/SettingsContext.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <SettingsProvider>
+        <MatchesProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </MatchesProvider>
+      </SettingsProvider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
+
